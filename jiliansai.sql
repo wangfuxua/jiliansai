9bad38b81df46a405852cabad30a554a709989d0
wangfuxu <446771190@qq.com>
2016-07-13 19:17:59 +0800

    jls

--- jiliansai.sql
+++ jiliansai.sql
@@ -10,10 +10,36 @@ Target Server Type    : MYSQL
 Target Server Version : 50612
 File Encoding         : 65001
 
-Date: 2016-07-12 19:17:14
+Date: 2016-07-13 19:17:24
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for jls_admin_colmns
+-- ----------------------------
+DROP TABLE IF EXISTS `jls_admin_colmns`;
+CREATE TABLE `jls_admin_colmns` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `type` int(11) NOT NULL DEFAULT '1' COMMENT '分级',
+  `fid` int(11) NOT NULL DEFAULT '0' COMMENT '父栏目名',
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '是否显示',
+  `cls_name` varchar(255) NOT NULL COMMENT '栏目标识-对应类',
+  `name` varchar(60) NOT NULL COMMENT '栏目名/操作名',
+  `url` varchar(120) NOT NULL COMMENT '链接',
+  `timeline` int(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of jls_admin_colmns
+-- ----------------------------
+INSERT INTO `jls_admin_colmns` VALUES ('1', '1', '0', '1', 'item', '项目管理', '#', '0');
+INSERT INTO `jls_admin_colmns` VALUES ('2', '2', '1', '1', 'item', '项目列表', 'item/ItemList', '0');
+INSERT INTO `jls_admin_colmns` VALUES ('3', '2', '1', '1', 'item', '项目添加', 'item/additem', '0');
+INSERT INTO `jls_admin_colmns` VALUES ('4', '1', '0', '1', 'news', '新闻中心', '0', '0');
+INSERT INTO `jls_admin_colmns` VALUES ('5', '2', '4', '1', 'news', '新闻列表', 'news/list', '0');
+INSERT INTO `jls_admin_colmns` VALUES ('6', '2', '4', '1', 'news', '添加新闻', 'news/addnews', '0');
 
 -- ----------------------------
 -- Table structure for jls_admin_users
@@ -33,7 +59,46 @@ CREATE TABLE `jls_admin_users` (
 -- ----------------------------
 -- Records of jls_admin_users
 -- ----------------------------
-INSERT INTO `jls_admin_users` VALUES ('1', 'admin', 'admin', '0', null, '0', '0');
+INSERT INTO `jls_admin_users` VALUES ('1', 'admin', '$P$BO/QQT2cxae1JLfAQY.jcn0FzFe1YD/', '0', null, '0', '0');
+
+-- ----------------------------
+-- Table structure for jls_channels
+-- ----------------------------
+DROP TABLE IF EXISTS `jls_channels`;
+CREATE TABLE `jls_channels` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '0' COMMENT '合作名称',
+  `url` varchar(255) NOT NULL DEFAULT '0' COMMENT '合作链接',
+  `cln_id` int(11) NOT NULL DEFAULT '0',
+  `desc` varchar(255) NOT NULL COMMENT '介绍',
+  `type` int(11) NOT NULL DEFAULT '1' COMMENT '是否跳转',
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '是否显示',
+  `timeline` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of jls_channels
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for jls_colmns
+-- ----------------------------
+DROP TABLE IF EXISTS `jls_colmns`;
+CREATE TABLE `jls_colmns` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `fid` int(11) NOT NULL DEFAULT '0' COMMENT '父级id',
+  `name` varchar(255) NOT NULL COMMENT '栏目名称',
+  `status` int(11) NOT NULL DEFAULT '1' COMMENT '状态',
+  `timeline` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of jls_colmns
+-- ----------------------------
+INSERT INTO `jls_colmns` VALUES ('1', '0', '新闻中心', '1', '0');
+INSERT INTO `jls_colmns` VALUES ('2', '1', '战报', '1', '0');
 
 -- ----------------------------
 -- Table structure for jls_fights
@@ -65,7 +130,7 @@ CREATE TABLE `jls_games` (
   `stime` int(11) NOT NULL COMMENT '开始报名时间',
   `turn` int(11) NOT NULL DEFAULT '1' COMMENT '轮次',
   `etime` int(11) NOT NULL DEFAULT '0' COMMENT '报名结束时间',
-  `verify_id` varchar(255) NOT NULL DEFAULT '0' COMMENT '验证方式',
+  `verify_id` varchar(30) NOT NULL DEFAULT '0' COMMENT '验证方式',
   `timeline` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -83,6 +148,7 @@ CREATE TABLE `jls_groups` (
   `game_id` int(11) NOT NULL,
   `group` varchar(255) NOT NULL COMMENT '所属组 例如：1',
   `turn` int(11) NOT NULL DEFAULT '1' COMMENT '轮次',
+  `status` int(2) NOT NULL DEFAULT '0' COMMENT '是否已经分配比赛',
   `tid` int(11) NOT NULL COMMENT '队伍id',
   `timeline` int(11) NOT NULL,
   PRIMARY KEY (`id`)
@@ -114,6 +180,24 @@ INSERT INTO `jls_items` VALUES ('1', '英雄联盟', 'attachment/game/head/16/07
 INSERT INTO `jls_items` VALUES ('2', '王者荣耀', 'attachment/game/head/16/07/20160712133701933.jpg', '王者荣耀', '1', '1', '0');
 
 -- ----------------------------
+-- Table structure for jls_news
+-- ----------------------------
+DROP TABLE IF EXISTS `jls_news`;
+CREATE TABLE `jls_news` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL COMMENT '文章标题',
+  `desc` varchar(255) NOT NULL COMMENT '描述',
+  `text` text COMMENT '文章内容',
+  `cln_id` int(11) NOT NULL COMMENT '所属栏目',
+  `timeline` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of jls_news
+-- ----------------------------
+
+-- ----------------------------
 -- Table structure for jls_pnums
 -- ----------------------------
 DROP TABLE IF EXISTS `jls_pnums`;
@@ -123,7 +207,7 @@ CREATE TABLE `jls_pnums` (
   `beizhu` varchar(255) NOT NULL COMMENT '备注 ',
   `timeline` int(11) NOT NULL DEFAULT '0' COMMENT '时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of jls_pnums
@@ -131,6 +215,9 @@ CREATE TABLE `jls_pnums` (
 INSERT INTO `jls_pnums` VALUES ('1', '1', '1v1', '0');
 INSERT INTO `jls_pnums` VALUES ('2', '2', '2v2', '0');
 INSERT INTO `jls_pnums` VALUES ('3', '3', '3v3', '0');
+INSERT INTO `jls_pnums` VALUES ('4', '4', '4v4', '0');
+INSERT INTO `jls_pnums` VALUES ('5', '5', '5v5', '0');
+INSERT INTO `jls_pnums` VALUES ('6', '6', '6v6', '0');
 
 -- ----------------------------
 -- Table structure for jls_teams
@@ -180,8 +267,11 @@ CREATE TABLE `jls_vervitys` (
   `name` varchar(30) NOT NULL COMMENT '验证方式名称',
   `timeline` int(11) NOT NULL COMMENT '时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of jls_vervitys
 -- ----------------------------
+INSERT INTO `jls_vervitys` VALUES ('1', '手机', '0');
+INSERT INTO `jls_vervitys` VALUES ('2', '身份证', '0');
+INSERT INTO `jls_vervitys` VALUES ('3', '学生证', '0');
