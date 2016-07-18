710b1b1a61701255ab2838bd6aec3e8d5a60812b
wangfuxua <44677190@qq.com>
2016-07-18 21:58:19 +0800

    jls

--- jiliansaiziliao/季联赛/季联赛网页/files/街头篮球/data.js
+++
@@ -1,7 +0,0 @@
-﻿$axure.loadCurrentPage(
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,c,d,e,f,g,h,[i,j,k,l,m,n],o,_(p,q,r,s,t,u,v,_(),w,_(x,y,z,A,B,_(C,D,E,F),G,null,H,A,I,A,J,K,L,null,M,N,O,P,Q,R,S,N),T,_(),U,_(),V,_(W,[])),X,_(),Y,_());}; 
-var b="url",c="街头篮球.html",d="generationDate",e=new Date(1467431065982.16),f="isCanvasEnabled",g=false,h="variables",i="OnLoadVariable",j="youxi",k="tupian",l="sjh",m="zhanghao",n="dlcg",o="page",p="packageId",q="988b2a31a2694fb991cbaca6b5f5dac2",r="type",s="Axure:Page",t="name",u="街头篮球",v="notes",w="style",x="baseStyle",y="627587b6038d43cca051c114ac41ad32",z="pageAlignment",A="near",B="fill",C="fillType",D="solid",E="color",F=0xFFFFFFFF,G="image",H="imageHorizontalAlignment",I="imageVerticalAlignment",J="imageRepeat",K="auto",L="favicon",M="sketchFactor",N="0",O="colorStyle",P="appliedColor",Q="fontName",R="Applied Font",S="borderWidth",T="adaptiveStyles",U="interactionMap",V="diagram",W="objects",X="masters",Y="objectPaths";
-return _creator();
-})());
