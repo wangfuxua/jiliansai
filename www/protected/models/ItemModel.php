6fd50025feac95d45f4c7b65a2c5fe319d7d4136
wangfuxua <44677190@qq.com>
2016-07-24 16:29:14 +0800

    jls

--- www/protected/models/ItemModel.php
+++ www/protected/models/ItemModel.php
@@ -15,6 +15,19 @@ class ItemModel extends CommonModel{
      *获取比赛需要的验证项目
      * */
 
+    function GetVers($gameid){
+        $sql="select b.* from `jls_games` as a RIGHT  JOIN  `jls_vervitys` as b on a.verify_ud=b.id where a.id={$gameid}";
+
+            return Yii::app()->db->createCommand($sql)->queryAll();
+    }
+    /*
+     * 获取项目下的所有比赛
+     * */
+    function GetGaems($itemid){
+        $sql="select * from  `jls_games` where item_id={$itemid}";
+        return Yii::app()->db->createCommand($sql)->queryAll();
+    }
+    
 
 }
 
