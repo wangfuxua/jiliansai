a748f758a3ae81a2513b2b7e642c38a679b3ec27
wangfuxua <44677190@qq.com>
2016-08-04 02:01:10 +0800

    jls

--- admin/protected/models/GroupModel.php
+++ admin/protected/models/GroupModel.php
@@ -72,5 +72,59 @@ class GroupModel extends CommonModel{
         $sql="select `turn` from `jls_games` where id={$gameid}";
         return Yii::app()->db->createCommand($sql)->queryScalar();
     }
+    /*
+     * 添加比赛
+     * */
+    function AddFight($data){
+        $fi['teamA']=$data['teama'];
+        $fi['teamB']=$data['teamb'];
+        $fi['starttime']=$data['stime'];
+        $fi['group_id']=$data['groupid'];
+        $fi['turn']=$data['turn'];
+        $fi['timeline']=time();
+        return $this->addData('jls_fights',$fi);
+    }
+   /*
+    * 获取比赛
+    * */
+    function GetFi($group_id,$turn){
+
+        $sql="select * from `jls_fights`  where turn={$turn} ADD  group_id={$group_id}";
+        $data= Yii::app()->db->createCommand($sql)->queryALl();
+        foreach ($data as $k=>$v){
+            $data[$k]['Tainfo']=$this->GetTname($v['teamA']);
+            $data[$k]['Tbinfo']=$this->GetTname($v['teamB']);
+
+        }
+        return $data;
+    }
+    /*
+     * 获取队伍名称
+     * */
+        function GetTname($tid){
+            $sql="select * from `jls_teams` where id={$tid}";
+            return Yii::app()->db->createCommand($sql)->queryRow();
+        }
+    /*
+     * 添加比赛胜利队伍
+     * */
+    function AddWinT($data){
+        $wi['fid']=$data['fid'];//胜利队伍所书的对战id
+        $wi['turn']=$data['turn'];//队伍进入到的轮次
+        $wi['gameid']=$data['gameid'];//队伍所属的比赛
+        $wi['teamid']=$data['teamid'];
+        $wi['timeline']=time();
+        return $this->addData('jls_winteam',$wi);
+    }
+    /*
+     * h获取某个小组的队伍信息
+     * */
+    function GetTinfoByG($gameid,$turn,$group){
+            $sql="select * from `jls_groups` where game_id={$gameid} and turn={$turn} and group={$gameid}";
+            $data=Yii::app()->db->createCommand($sql)->queryAll();
+
+        return $data;
+
+    }
 
 }
